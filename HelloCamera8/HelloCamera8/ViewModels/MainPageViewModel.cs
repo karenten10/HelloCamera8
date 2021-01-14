4bd1c1716d12396207210cfec6332d1ed885d6b8
Karen <karenten10@gmail.com>
2021-01-14 15:16:04 +0800

    add commercial logic

--- HelloCamera8/HelloCamera8/ViewModels/MainPageViewModel.cs
+++ HelloCamera8/HelloCamera8/ViewModels/MainPageViewModel.cs
@@ -8,12 +8,42 @@ using System.Text;
 
 namespace HelloCamera8.ViewModels
 {
-    public class MainPageViewModel : ViewModelBase
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly INavigationService navigationService;
+
+        public string DisplayString { get; set; } = "Hi";
+
+        public DelegateCommand ClickButton { get; set; }
+
         public MainPageViewModel(INavigationService navigationService)
-            : base(navigationService)
         {
-            Title = "Main Page";
+            this.navigationService = navigationService;
+
+            ClickButton = new DelegateCommand(() =>
+            {
+                DisplayString = "Hello";
+            });
+
         }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
     }
 }
