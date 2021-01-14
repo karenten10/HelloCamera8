1de5a746081660db2c604a7a56f441aaa84ba802
Karen <karenten10@gmail.com>
2021-01-14 15:30:57 +0800

    zip apk

--- HelloCamera8/HelloCamera8.Android/MainApplication.cs
+++ HelloCamera8/HelloCamera8.Android/MainApplication.cs
@@ -4,9 +4,22 @@ using Android.Runtime;
 
 namespace HelloCamera8.Droid
 {
-    [Application(
-        Theme = "@style/MainTheme"
+    //[Application(
+    //    Theme = "@style/MainTheme"
+    //    )]
+
+#if DEBUG
+[Application(
+        Theme = "@style/MainTheme",
+        Debuggable = true
         )]
+#else
+    [Application(
+        Theme = "@style/MainTheme",
+        Debuggable = false
+        )]
+#endif
+
     public class MainApplication : Application
     {
         public MainApplication(IntPtr javaReference, JniHandleOwnership transfer)
